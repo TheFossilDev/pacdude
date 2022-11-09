94b9dc8ea7e2967d5faa405dfb4567deab57d87e
Cole <colewestbrookphoto@gmail.com>
2022-11-08 19:05:27 -0500

    WIP

--- PlayPacDude.py
+++ PlayPacDude.py
@@ -1,7 +1,29 @@
-from Level import *
-def main():
-  testLevel = Level()
-  testLevel.generateMapFromFile("pacmapdemo.txt")
-  testLevel.printMapToConsole()
+import pygame
+import os
+pygame.font.init()
+pygame.mixer.init()
 
-main()
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Game!")
+FPS = 60
+
+def DrawLevel():
+  pass
+
+
+def main():
+  clock = pygame.time.Clock()
+  run = True
+  while run:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+      print(event)
+      if event.type == pygame.QUIT:
+        run = False
+  pygame.quit()
+
+
+
+if __name__ == "__main__":
+  main()
